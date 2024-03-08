ee3c4bd64aa68a84d7fc86bc74fd3513f77cfe1a
unknown <ofarukistikbal@hotmail.com>
2024-03-08 20:17:50 +0300

    Move logic from Views to ViewModels

--- DX12Editor/ViewModels/ProjectBrowserDialogViewModel.cs
+++ DX12Editor/ViewModels/ProjectBrowserDialogViewModel.cs
@@ -1,18 +1,43 @@
 ﻿using Avalonia.Controls;
 using DX12Editor.Views;
 using ReactiveUI;
-using System.Globalization;
+using System.Reactive;
 
 namespace DX12Editor.ViewModels
 {
     public class ProjectBrowserDialogViewModel : ViewModelBase
     {
-        private UserControl _borderContent;
+        private UserControl? _borderContent;
+        private NewProject _newProject;
+        private OpenProject _openProject;
+
+        public ReactiveCommand<Unit, Unit> ProjectOpenButton { get; }
+        public ReactiveCommand<Unit, Unit> ProjectCreateButton { get; }
+
+        public ProjectBrowserDialogViewModel()
+        {
+            ProjectOpenButton = ReactiveCommand.Create(OnProjectOpenButton);
+            ProjectCreateButton = ReactiveCommand.Create(OnProjectCreateButton);
+
+            _newProject = new NewProject();
+            _openProject = new OpenProject();
+            BorderContent = _openProject;
+        }
 
         public UserControl BorderContent
         {
             get => _borderContent;
             set => this.RaiseAndSetIfChanged(ref _borderContent, value);
+        }
+
+        private void OnProjectCreateButton()
+        {
+            BorderContent = _newProject;
+        }
+
+        private void OnProjectOpenButton()
+        {
+            BorderContent = _openProject;
         }
     }
 }
--- DX12Editor/Views/ProjectBrowserDialog.axaml.cs
+++ DX12Editor/Views/ProjectBrowserDialog.axaml.cs
@@ -1,8 +1,5 @@
-using Avalonia;
 using Avalonia.Controls;
-using Avalonia.Markup.Xaml;
 using DX12Editor.ViewModels;
-using System.Diagnostics;
 
 
 namespace DX12Editor.Views
@@ -10,29 +7,12 @@ namespace DX12Editor.Views
     public partial class ProjectBrowserDialog : Window
     {
         private ProjectBrowserDialogViewModel _viewModel;
-        private NewProject _newProject;
-        private OpenProject _openProject;
 
         public ProjectBrowserDialog()
         {
             InitializeComponent();
             _viewModel = new ProjectBrowserDialogViewModel();
-            _newProject = new NewProject();
-            _openProject = new OpenProject();
             DataContext = _viewModel;
-        }
-
-        private void OnProjectToggleButtonClick(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
-        {
-            if (sender == openProjectButton)
-            {
-                _viewModel.BorderContent = _openProject;
-            }
-            else if (sender == createProjectButton)
-            {
-                _viewModel.BorderContent = _newProject;
-            }
-
         }
     }
 }
