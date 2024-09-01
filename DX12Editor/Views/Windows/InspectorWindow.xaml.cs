09a8e189b0c03a2fac79e92f4aac301be9142c39
Omer Faruk Istikbal <ofarukistikbal@hotmail.com>
2024-09-01 21:47:05 +0300

    Add Inspector

---
+++ DX12Editor/Views/Windows/InspectorWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using DX12Editor.Attributes;
+using DX12Editor.ViewModels.Windows;
+
+namespace DX12Editor.Views.Windows
+{
+    /// <summary>
+    /// Interaction logic for InspectorWindow.xaml
+    /// </summary>
+    /// 
+    [Window("Inspector", typeof(InspectorWindowViewModel))]
+    public partial class InspectorWindow : UserControl
+    {
+        public InspectorWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
